5a2b02e1b8864f74ce8a43419f8ad0739706bd5e
Dan Luu <danluu@gmail.com>
2013-03-05 16:34:37 -0500

    try to decode tracker response (seems to be missing a byte at the end)

--- src/main/scala/snippets.scala
+++ src/main/scala/snippets.scala
@@ -5,6 +5,9 @@ import scalaj.http.Http
 import collection.immutable.ListMap
 import java.net.URLEncoder
 
+import scala.util.parsing.combinator._
+import scala.util.parsing.input._
+
 object Snippets {
   def main(args: Array[String]) {
     //    val metainfoStream  = Resource.fromFile("tom.torrent").mkString
@@ -12,7 +15,7 @@ object Snippets {
     val metainfo = source.mkString
     source.close()
     val decodedMeta = BencodeDecoder.decode(metainfo)
-    //    println(decodedMeta)
+    println(s"decoded torrent ${decodedMeta}")
 
     //    decodedMeta.get.foreach{x => println(s"ITEM: ${x}")}
     val metaMap = decodedMeta.get match {
@@ -36,19 +39,20 @@ object Snippets {
     val infoSHAEncoded = hexStringURLEncode(infoSHA)
     println(infoSHAEncoded)
 
-    val params = Map("port"->"63211", "uploaded"->"0","downloaded"->"0", "left"->"1277987")
-    val encodedParams = (for ((k, v) <- params) yield URLEncoder.encode(k) + "=" + URLEncoder.encode(v) ).mkString("&")
-  //  val encodedParams = URLEncoder.encode(infoSHAASCII, "UTF-8")
-//    println(s"${infoSHA} encoded to ${encodedParams} (${infoSHAASCII})")
+    val params = Map("port" -> "63211", "uploaded" -> "0", "downloaded" -> "0", "left" -> "1277987")
+    val encodedParams = (for ((k, v) <- params) yield URLEncoder.encode(k) + "=" + URLEncoder.encode(v)).mkString("&")
+    //  val encodedParams = URLEncoder.encode(infoSHAASCII, "UTF-8")
+    //    println(s"${infoSHA} encoded to ${encodedParams} (${infoSHAASCII})")
     val infoSHAParam = s"info_hash=${infoSHAEncoded}"
-    val peerIdParam = s"peer_id=${infoSHAEncoded}"//FIXME: peer id should obviously not be the same as our hash
+    val peerIdParam = s"peer_id=${infoSHAEncoded}" //FIXME: peer id should obviously not be the same as our hash
     val allParams = s"?${infoSHAParam}&${peerIdParam}&${encodedParams}"
-
 
     println(s"sending ${allParams}")
     //IP seems to be 67.215.65.132
     val trackerResponse = Http("http://thomasballinger.com:6969/announce" + allParams).asString
     //    val trackerResponse = Http("http://thomasballinger.com:6969/announce").asString
+    val decodedTrackerResponse = BencodeDecoder.decode(trackerResponse)
     println(trackerResponse)
+    println(decodedTrackerResponse)
   }
 }
