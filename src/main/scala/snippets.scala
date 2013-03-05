ec05c613346266927c3ed29644ecc7f7564cdff9
Dan Luu <danluu@gmail.com>
2013-03-05 14:59:20 -0500

    SHA1, incorrect

--- src/main/scala/snippets.scala
+++ src/main/scala/snippets.scala
@@ -3,6 +3,7 @@ package org.storrent
 import org.saunter.bencode._
 import scalaj.http.Http
 import collection.immutable.ListMap
+import java.net.URLEncoder
 
 object Snippets{
   def main(args: Array[String]){
@@ -15,12 +16,24 @@ object Snippets{
 
 //    decodedMeta.get.foreach{x => println(s"ITEM: ${x}")}
     val metaMap = decodedMeta.get match {
-      case m: Map[String,String] => m
+      case m: Map[Any,Any] => m
       case _ => throw new ClassCastException
     }
 
-    println(metaMap)
+    val infoMap = metaMap.get("info").get
+    val encodedInfoMap = BencodeEncoder.encode(infoMap)
+//    val encodedInfoMap = BencodeEncoder.encode(List("foo","bar"))
+//    println(encodedInfoMap)
 
+    val md = java.security.MessageDigest.getInstance("SHA-1")
+    val infoSHA = new sun.misc.BASE64Encoder().encode(md.digest(encodedInfoMap.getBytes))
+
+    val params = Map("info_hash"->infoSHA)
+
+    val encodedParams = (for ((k, v) <- params) yield URLEncoder.encode(k) + "=" + URLEncoder.encode(v) ).mkString("&")
+    println(s"${infoSHA} encoded to ${encodedParams}")
+
+    //IP seems to be 67.215.65.132
 //    val trackerResponse = Http("http://thomasballinger.com:6969/announce").asString
 //    println(trackerResponse)
   }
