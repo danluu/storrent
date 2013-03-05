864186ed6d583a05029af5637e1e2ceefb861b18
Dan Luu <danluu@gmail.com>
2013-03-05 15:54:44 -0500

    URLEncoded SHA1

--- src/main/scala/snippets.scala
+++ src/main/scala/snippets.scala
@@ -28,46 +28,18 @@ object Snippets {
     val md = java.security.MessageDigest.getInstance("SHA-1")
     val infoSHA = md.digest(encodedInfoMap.getBytes).map(0xFF & _).map { "%02x".format(_) }.foldLeft("") { _ + _ } //taken from Play
 
-    /*
-    def toAscii(hex: String) = {
-      import javax.xml.bind.DatatypeConverter
-      new String(DatatypeConverter.parseHexBinary(hex))
+    //take a string that's already in hex and URLEncode it by putting a % in front of each pair
+    def hexStringURLEncode(x: String) = {
+      x.grouped(2).toList.map("%" + _).mkString("")
     }
- */
 
-/*
-    def toAscii(hex: String) = {
-      require(hex.size % 2 == 0,
-        "Hex must have an even number of characters. You had " + hex.size)
-      val sb = new StringBuilder
-      for (i <- 0 until hex.size by 2) {
-        val str = hex.substring(i, i + 2)
-        sb.append(Integer.parseInt(str, 16).toChar)
-
-        val convert = Integer.parseInt(str,16).toChar
-//        println(s"${str} converts to ${convert} encodes to ${URLEncoder.encode(convert.toString())}")
-      }
-      sb.toString
-    }
- */
-
-/*
-    def wtfAscii() = {
-      for (i <- 0 until 255){
-        val str = i.toString()
-        val convert = Integer.parseInt(str,16).toChar
-        println(s"${str} converts to ${convert} encodes to ${URLEncoder.encode(convert.toString())}")
-      }
-    }
-    wtfAscii()
-
- */
-    val infoSHAASCII = toAscii(infoSHA)
+    val infoSHAEncoded = hexStringURLEncode(infoSHA)
+    println(infoSHAEncoded)
 
     //    val params = Map("info_hash"->infoSHA)
     //    val encodedParams = (for ((k, v) <- params) yield URLEncoder.encode(k) + "=" + URLEncoder.encode(v) ).mkString("&")
-    val encodedParams = URLEncoder.encode(infoSHAASCII, "UTF-8")
-    println(s"${infoSHA} encoded to ${encodedParams} (${infoSHAASCII})")
+  //  val encodedParams = URLEncoder.encode(infoSHAASCII, "UTF-8")
+//    println(s"${infoSHA} encoded to ${encodedParams} (${infoSHAASCII})")
 
     //IP seems to be 67.215.65.132
     //    val trackerResponse = Http("http://thomasballinger.com:6969/announce").asString
