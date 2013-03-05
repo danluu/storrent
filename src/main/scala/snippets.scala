44d7c3ea952a28bd35812b0383019582adbad157
Dan Luu <danluu@gmail.com>
2013-03-05 13:52:27 -0500

    pull out hash from torrent

--- src/main/scala/snippets.scala
+++ src/main/scala/snippets.scala
@@ -1,6 +1,8 @@
 package org.storrent
 
 import org.saunter.bencode._
+import scalaj.http.Http
+import collection.immutable.ListMap
 
 object Snippets{
   def main(args: Array[String]){
@@ -9,6 +11,17 @@ object Snippets{
     val metainfo = source.mkString
     source.close()
     val decodedMeta = BencodeDecoder.decode(metainfo)
-    println(decodedMeta)
+//    println(decodedMeta)
+
+//    decodedMeta.get.foreach{x => println(s"ITEM: ${x}")}
+    val metaMap = decodedMeta.get match {
+      case m: Map[String,String] => m
+      case _ => throw new ClassCastException
+    }
+
+    println(metaMap)
+
+//    val trackerResponse = Http("http://thomasballinger.com:6969/announce").asString
+//    println(trackerResponse)
   }
 }
