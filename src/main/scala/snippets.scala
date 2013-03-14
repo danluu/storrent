206225d530aac57160317b659494f03e8ff9b493
Dan Luu <danluu@gmail.com>
2013-03-14 16:45:37 -0400

    refactor network receive and correctly handle multi-packet messages

--- src/main/scala/snippets.scala
+++ src/main/scala/snippets.scala
@@ -51,16 +51,16 @@ class BigFIXMEObject extends Actor with ActorLogging {
       source.close()
       val decodedMeta = BencodeDecoder.decode(metainfo)
 
-//      println(s"decoded torrent ${decodedMeta}")
+      //      println(s"decoded torrent ${decodedMeta}")
 
       //    decodedMeta.get.foreach{x => println(s"ITEM: ${x}")}
       val metaMap = decodedMeta.get match {
         case m: Map[String, Any] => m
         case m => println(m.getClass.getSimpleName); throw new ClassCastException
-      } 
+      }
 
-      val infoMap = metaMap.get("info").get  match{
-        case m: Map[String,Any] => m
+      val infoMap = metaMap.get("info").get match {
+        case m: Map[String, Any] => m
         case m => println(m.getClass.getSimpleName); throw new ClassCastException
       }
       val encodedInfoMap = BencodeEncoder.encode(infoMap)
@@ -75,7 +75,7 @@ class BigFIXMEObject extends Actor with ActorLogging {
         case m: Long => m
         case m => println(m.getClass.getSimpleName); throw new ClassCastException
       }
-//       println(infoMap.get("piece length").get)
+      //       println(infoMap.get("piece length").get)
       val sparePiece = fileLength % pieceLength match {
         case 0 => 0
         case _ => 1
@@ -87,7 +87,7 @@ class BigFIXMEObject extends Actor with ActorLogging {
       val md = java.security.MessageDigest.getInstance("SHA-1")
       val infoSHABytes = md.digest(encodedInfoMap.getBytes).map(0xFF & _)
       val infoSHA = infoSHABytes.map { "%02x".format(_) }.foldLeft("") { _ + _ } //taken from Play
-//      println(s"hash into string: ${infoSHA}")
+      //      println(s"hash into string: ${infoSHA}")
 
       //take a string that's already in hex and URLEncode it by putting a % in front of each pair
       def hexStringURLEncode(x: String) = {
@@ -95,7 +95,7 @@ class BigFIXMEObject extends Actor with ActorLogging {
       }
 
       val infoSHAEncoded = hexStringURLEncode(infoSHA)
-//      println(infoSHAEncoded)
+      //      println(infoSHAEncoded)
 
       val params = Map("port" -> "63211", "uploaded" -> "0", "downloaded" -> "0", "left" -> "1277987")
       val encodedParams = (for ((k, v) <- params) yield URLEncoder.encode(k) + "=" + URLEncoder.encode(v)).mkString("&")
@@ -106,7 +106,7 @@ class BigFIXMEObject extends Actor with ActorLogging {
       val allParams = s"?${infoSHAParam}&${peerIdParam}&${encodedParams}"
 
       val completeUrl = "http://thomasballinger.com:6969/announce" + allParams
-//      println(s"sending ${allParams}")
+      //      println(s"sending ${allParams}")
       //IP seems to be 67.215.65.132
 
       val url = new URL(completeUrl)
@@ -115,7 +115,7 @@ class BigFIXMEObject extends Actor with ActorLogging {
       //    println(content.split(":").last.toCharArray.map(_.toByte).mkString(",")) //this was a highly upvoted, but wrong, stackoverflow suggestion
 
       val decodedTrackerResponse = BencodeDecoder.decode(trackerResponse)
-//      println(trackerResponse)
+      //      println(trackerResponse)
       //    println(decodedTrackerResponse)
 
       val someTrackerResponse = decodedTrackerResponse.get match {
@@ -141,7 +141,7 @@ class BigFIXMEObject extends Actor with ActorLogging {
         peers.foreach(x => println(x.mkString(".")))
         val ips = peers.map(x => x.slice(0, 4).mkString("."))
         val ports = peers.map { x =>
-//          println(s"port calculation: ${x(4)}, ${x(5)}, result = ${(x(4) << 4) + x(5)}")
+          //          println(s"port calculation: ${x(4)}, ${x(5)}, result = ${(x(4) << 4) + x(5)}")
           (x(4) << 8) + x(5)
         }
         //      println(s"ips: ${ips}")
@@ -172,9 +172,84 @@ class TCPServer() extends Actor with ActorLogging {
 
   var subserver: ActorRef = self //FIXME: should make this a val that we initialize when the class is instantiated
   var handshakeSeen: Boolean = false
+  var TcpReadBuffer: ByteString = akka.util.ByteString()
+  var messageReader = handshakeReader _
   val hasPiece: scala.collection.mutable.Set[Int] = scala.collection.mutable.Set() //inefficient representation
   val weHavePiece: scala.collection.mutable.Set[Int] = scala.collection.mutable.Set()
   //FIXME: need a way to specify that we're currently downloading and should not request again
+
+  def handshakeReader(LocalBuffer: ByteString): Int = {
+    if (LocalBuffer.length < 68) {
+      0
+    } else {
+      subserver ! SendInterested
+      messageReader = parseFrame
+      68
+    }
+  }
+
+  def parseFrame(localBuffer: ByteString): Int = {
+    if (localBuffer.length < 4)
+      return 0
+    val lengthBytes = localBuffer.take(4)
+    val length = fourBytesToInt(lengthBytes)
+    if (length > localBuffer.length - 4)
+      return 0
+
+    val message = localBuffer.drop(4).take(length)
+
+    def processMessage(m: ByteString) {
+      val rest = m.drop(1)
+      m(0) & 0xFF match {
+        case 1 => //UNCHOKE
+          println("UNCHOKE")
+          val missing = hasPiece -- weHavePiece
+          subserver ! GetPiece(missing.head)
+        case 4 => //HAVE piece
+          val index = fourBytesToInt(rest.take(4))
+          println(s"HAVE ${index}")
+          hasPiece += index
+        case 5 => //BITFIELD
+          println(s"BITFIELD")
+          def bitfieldToSet(index: Int): Unit = {
+            //goes through each byte, and calls a function which goes through each bit and converts MSB:0 -> LSB:N in Set
+            def byteToSet(byte: Byte, index: Int) = {
+              def bitToSet(bit_index: Int): Unit = {
+                //                    println(s"bitToSet with bit_index ${bit_index}. byte: ${byte} & ${1 << bit_index} = ${byte & (1 << bit_index)}")
+                if ((byte & (1 << bit_index)) != 0) {
+                  //                      println(s"adding ${8 * index + (7 - bit_index)}")
+                  hasPiece += 8 * index + (7 - bit_index)
+                }
+                if (bit_index > 0) {
+                  bitToSet(bit_index - 1)
+                }
+              }
+              bitToSet(7)
+            }
+            byteToSet(rest.drop(index)(0), index)
+
+            val newIndex = index + 1
+            if (newIndex < rest.length)
+              bitfieldToSet(newIndex)
+          }
+          bitfieldToSet(0)
+
+          println(s"hasPiece: ${hasPiece}")
+        case 7 => //PEICE
+          println(s"PEICE ${rest.take(4)}")
+          val index = fourBytesToInt(rest.take(4))
+          weHavePiece += index
+          val missing = hasPiece -- weHavePiece
+          subserver ! GetPiece(missing.head)
+      }
+    }
+    processMessage(message)
+    return length + 4
+  }
+
+  def fourBytesToInt(bytes: IndexedSeq[Byte]): Int = {
+    (bytes(0) << 8 * 3) + (bytes(1) << 8 * 2) + (bytes(2) << 8) + bytes(3)
+  }
 
   val serverSocket = IOManager(context.system).listen("0.0.0.0", 31733)
 
@@ -182,7 +257,7 @@ class TCPServer() extends Actor with ActorLogging {
     //FIXME: only passing info_hash in because we're putting the handshake here
     case ConnectToPeer(ip, port, info_hash, fileLength, pieceLength) =>
       val socket = IOManager(context.system) connect (ip, port) //Ip, port
-      subserver= context.actorOf(Props(new SubServer(socket)))
+      subserver = context.actorOf(Props(new SubServer(socket)))
       //FIXME: this handshake should probably live somewhere else
       val pstrlen: Array[Byte] = Array(19)
       val pstr = "BitTorrent protocol".getBytes
@@ -204,98 +279,19 @@ class TCPServer() extends Actor with ActorLogging {
     case IO.NewClient(server) =>
       log.info("New incoming client connection on server")
       val socket = server.accept()
-      //FIXME: we can't accept clients right now
-    case IO.Read(socket, bytes) =>
-//      log.info(s"Read data from ${socket}: ${bytes} (${bytes.length})")
-      var bytesRead = 0
-      if (!handshakeSeen){
-        if (bytes.length < 68)
-          throw new Exception("Only received part of a packet")
-        handshakeSeen = true
-        bytesRead += 68
-        subserver ! SendInterested
-      }
-//      val cmd = ascii(bytes)
-      if (bytes.length > bytesRead){
-        readMessage()
-      }
-
-
-
+    //FIXME: we can't accept clients right now
+    case IO.Read(_, bytes) =>
       //FIXME: this should use a fold and be general
-      def fourBytesToInt(bytes: IndexedSeq[Byte]): Int = {
-         (bytes(0) << 8*3) + (bytes(1) << 8*2) + (bytes(2) << 8) + bytes(3)
+      TcpReadBuffer = TcpReadBuffer ++ bytes
+
+      //      log.info(s"Read data from ${socket}: ${bytes} (${bytes.length})")
+      var bytesRead = 1
+      while (bytesRead > 0) {
+        bytesRead = messageReader(TcpReadBuffer)
+        TcpReadBuffer = TcpReadBuffer.drop(bytesRead)
       }
 
-      //WARNING: we're assuming that IO.read always returns complete messages. We'll get an exception here from the take if that's false
-      def readMessage(): Unit = {
-        val lengthBytes = bytes.drop(bytesRead).take(4)
-        val length = fourBytesToInt(lengthBytes)
-        if (length > (bytes.length - 4)){
-          throw new Exception("readMessage(): Only received part of a packet")
-        }
-        bytesRead += 4
-        val message = bytes.drop(bytesRead).take(length)
-        bytesRead += length
-
-//        println(s"Received message: ${message} (${length})")
-        def processMessage(m: ByteString){
-          val rest = m.drop(1)
-          m(0) & 0xFF match {
-            case 1 => //UNCHOKE
-              println("UNCHOKE")
-              val missing = hasPiece -- weHavePiece
-              subserver ! GetPiece(missing.head)
-            case 4 =>  //HAVE piece
-              val index = fourBytesToInt(rest.take(4))
-              println(s"HAVE ${index}")
-              hasPiece += index
-            case 5 => //BITFIELD
-              println(s"BITFIELD")
-              def bitfieldToSet(index: Int): Unit = {
-                //goes through each byte, and calls a function which goes through each bit and converts MSB:0 -> LSB:N in Set
-                def byteToSet(byte: Byte, index: Int) = {
-                  def bitToSet(bit_index: Int): Unit = {
-//                    println(s"bitToSet with bit_index ${bit_index}. byte: ${byte} & ${1 << bit_index} = ${byte & (1 << bit_index)}")
-                    if ((byte & (1 << bit_index)) != 0){
-//                      println(s"adding ${8 * index + (7 - bit_index)}")
-                      hasPiece += 8 * index + (7 - bit_index)
-                    }
-                    if (bit_index > 0){
-                      bitToSet(bit_index - 1)
-                    }
-                  }
-                  bitToSet(7)
-                }
-                byteToSet(rest.drop(index)(0), index)
-
-              val newIndex = index + 1
-              if (newIndex < rest.length)
-                bitfieldToSet(newIndex)
-              }
-              bitfieldToSet(0)
-
-              println(s"hasPiece: ${hasPiece}")
-            case 7 => //PEICE
-              println(s"PEICE ${rest.take(4)}")
-              val index = fourBytesToInt(rest.take(4))
-              weHavePiece += index
-              val missing = hasPiece -- weHavePiece
-              subserver ! GetPiece(missing.head)
-          }
-        }
-
-        processMessage(message)
-
-        if (bytes.length > bytesRead){
-          readMessage()
-        }
-      }
-      
-
-
-//      log.info(s"Read data from ${socket}: ${cmd}")
-//      subservers(socket) ! NewMessage(cmd)
+    //WARNING: we're assuming that IO.read always returns complete messages. We'll get an exception here from the take if that's false
     case IO.Closed(socket, cause) =>
       context.stop(subserver)
       log.info(s"connection to ${socket} closed: ${cause}")
@@ -315,30 +311,28 @@ object TCPServer {
   case class SendInterested()
   case class GetPiece(index: Int)
 
-
-
   class SubServer(socket: IO.SocketHandle) extends Actor {
-//    var choked: Boolean = true
-//    var downloading: Boolean = false
+    //    var choked: Boolean = true
+    //    var downloading: Boolean = false
     var interested: Boolean = false
 
     def receive = {
       case SendInterested =>
-        if (! interested){
+        if (!interested) {
           println("Sending Interested message")
-          val msgAr: Array[Byte] = Array(0,0,0,1,2)
+          val msgAr: Array[Byte] = Array(0, 0, 0, 1, 2)
           val msg: ByteString = akka.util.ByteString.fromArray(msgAr, 0, msgAr.length)
           socket.write(msg)
         }
       case GetPiece(index) =>
-          //FIXME: this assumes the index < 256
-          //FIXME: hardcoding length because we know the file has piece size 16384
+        //FIXME: this assumes the index < 256
+        //FIXME: hardcoding length because we know the file has piece size 16384
         val msgAr: Array[Byte] =
-          Array(0,0,0,13, //len
+          Array(0, 0, 0, 13, //len
             6, //id
-            0,0,0,index.toByte, //index
-            0,0,0,0, //begin
-            0,0,0x40,0) //length = 16384
+            0, 0, 0, index.toByte, //index
+            0, 0, 0, 0, //begin
+            0, 0, 0x40, 0) //length = 16384
         val msg = akka.util.ByteString.fromArray(msgAr, 0, msgAr.length)
         println(s"sending request for piece: ${msg}")
         socket.write(msg)
