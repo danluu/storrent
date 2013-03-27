65643b3bb5f8244fb8b31a402055c3181129eec4
Dan Luu <danluu@gmail.com>
2013-03-27 12:24:31 -0400

    Move TCPClient to its own file

--- src/main/scala/PeerConnection.scala
+++ src/main/scala/PeerConnection.scala
@@ -1,47 +1,13 @@
 package org.storrent
 
-import akka.actor.{ Actor, ActorRef, IO, IOManager, ActorLogging, Props }
+import akka.actor.{ Actor, ActorRef, ActorLogging, Props }
 import akka.util.ByteString
 import akka.pattern.ask
-import akka.util._
+import akka.util.Timeout
 import scala.concurrent.duration._
 import scala.concurrent.Await
 
 import scala.concurrent.ExecutionContext.Implicits.global
-
-object TCPClient {
-  case class DataReceived(buffer: ByteString)
-  case class ConnectionClosed
-  case class CloseConnection
-  case class SendData(bytes: ByteString)
-}
-
-class TCPClient(ip: String, port: Int, peer: ActorRef) extends Actor with ActorLogging {
-  import TCPClient._
-
-  implicit val timeout = Timeout(5.seconds)
-  val socket = IOManager(context.system) connect (ip, port) //Ip, port
-  var buffer: ByteString = akka.util.ByteString()
-
-  def receive = {
-    case IO.Closed(socket, cause) =>
-      log.info(s"connection to ${socket} closed: ${cause}")
-      peer ! ConnectionClosed
-      socket.close
-    case IO.Read(_, bytes) =>
-      buffer = buffer ++ bytes
-      var bytesRead = 0
-      do {
-        bytesRead = Await.result(peer ? DataReceived(buffer), 5.seconds).asInstanceOf[Int]
-        buffer = buffer.drop(bytesRead)
-      } while (bytesRead > 0)
-    case SendData(bytes) =>
-      socket write bytes
-    case CloseConnection =>
-      peer ! ConnectionClosed
-      socket.close
-  }
-}
 
 class PeerConnection(ip: String, port: Int, fileManager: ActorRef, info_hash: Array[Int], fileLength: Long, pieceLength: Long) extends Actor with ActorLogging {
   import PeerConnection._
---
+++ src/main/scala/TCPClient.scala
@@ -0,0 +1,44 @@
+package org.storrent
+
+import akka.actor.{ Actor, ActorRef, IO, IOManager, ActorLogging, Props }
+import akka.util.ByteString
+import scala.concurrent.duration._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.Await
+
+object TCPClient {
+  case class DataReceived(buffer: ByteString)
+  case class ConnectionClosed
+  case class CloseConnection
+  case class SendData(bytes: ByteString)
+}
+
+class TCPClient(ip: String, port: Int, peer: ActorRef) extends Actor with ActorLogging {
+  import TCPClient._
+
+  implicit val timeout = Timeout(5.seconds)
+  val socket = IOManager(context.system) connect (ip, port) //Ip, port
+  var buffer: ByteString = akka.util.ByteString()
+
+  def receive = {
+    case IO.Closed(socket, cause) =>
+      log.info(s"connection to ${socket} closed: ${cause}")
+      peer ! ConnectionClosed
+      socket.close
+    case IO.Read(_, bytes) =>
+      buffer = buffer ++ bytes
+      var bytesRead = 0
+      do {
+        bytesRead = Await.result(peer ? DataReceived(buffer), 5.seconds).asInstanceOf[Int]
+        buffer = buffer.drop(bytesRead)
+      } while (bytesRead > 0)
+    case SendData(bytes) =>
+      socket write bytes
+    case CloseConnection =>
+      peer ! ConnectionClosed
+      socket.close
+  }
+}
+
+
