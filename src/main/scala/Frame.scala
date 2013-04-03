6822aaaa76fb765061066c13e2842feea2859579
Dan Luu <danluu@gmail.com>
2013-04-03 16:19:45 -0400

    BTProtocol: Allow full ints and indicies and stop hardcoding length

--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -9,18 +9,19 @@ object Frame {
     ByteString.fromArray(msgAr, 0, msgAr.length)
   }
 
+  def intToByte(n: Int, length: Int): ByteString = {
+    val bb = java.nio.ByteBuffer.allocate(length).putInt(n).flip.asInstanceOf[java.nio.ByteBuffer] //have to cast due to scala weirdness
+    ByteString(bb)
+  }
+
   def createPieceFrame(index: Int, pieceLength: Long): ByteString = {
-    //FIXME: this assumes the index < 256
-    //FIXME: hardcoding length because we know the file has piece size 16384
-    //      val indexBytes = java.nio.ByteBuffer.allocate(4)
-    //      val aBytes: Array[Byte] = Array(indexBytes.putInt(index))
-    val msgAr: Array[Byte] =
-      Array(0, 0, 0, 13, //len
-        6, //id
-        0, 0, 0, index.toByte, //index
-        0, 0, 0, 0, //begin
-        0, 0, 0x40, 0) //length = 16384
-    ByteString.fromArray(msgAr, 0, msgAr.length)
+    val headerLenB = intToByte(13, 4)
+    val headerIdB = ByteString(6)
+    val indexB = intToByte(index, 4)
+    val beginB = intToByte(0, 4) //FIXME: should be able to handle an offset
+    val lengthB = intToByte(pieceLength.toInt, 4) //FIXME: handle long lengths
+
+    headerLenB ++ headerIdB ++ indexB ++ beginB ++ lengthB
   }
 
   def createHandshakeFrame(info_hash: Array[Int]) = {
