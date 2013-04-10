ed1b0df0869e05e86366e15ffff7721a1875aed4
Dan Luu <danluu@gmail.com>
2013-04-10 15:49:59 -0400

    Tests for short frames

--- src/test/scala/FrameSpec.scala
+++ src/test/scala/FrameSpec.scala
@@ -1,11 +1,14 @@
 package org.storrent
 
 import org.scalatest._
+import akka.util.ByteString
 
 class FrameSpec extends FlatSpec with ShouldMatchers {
   import Frame._
 
   "parseFrame" should "parse short frames" in {
-    intToByte(0, 4) should equal (intToByte(0, 4))
+    parseFrame(intToByte(0, 4)) should equal ((4,None))
+    parseFrame(ByteString(2)) should equal ((0,None))
+    parseFrame(ByteString(2) ++ ByteString(11)) should equal ((0,None))
   }
 }
