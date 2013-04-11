bfef986ee326dc16bc2151e5c92d0a077612d7db
Dan Luu <danluu@gmail.com>
2013-04-11 12:36:53 -0400

    Rename ActorRef to match structure

--- src/main/scala/TCPClient.scala
+++ src/main/scala/TCPClient.scala
@@ -15,10 +15,9 @@ object TCPClient {
   def apply(ip: String, port: Int, peer: ActorRef) = new TCPClient(ip: String, port: Int, peer: ActorRef)
 }
 
-// FIXME: peer actorRef is actually a BTProtocol actorRef
 // FIXME: we may want to buffer more stuff in BTProtocol. "because buffering is part of parsing, not part of IO"
 // alternately, we should look at length directly, right here. BTProtocol can look at fully frame messages only
-class TCPClient(ip: String, port: Int, peer: ActorRef) extends Actor with ActorLogging {
+class TCPClient(ip: String, port: Int, btProtocol: ActorRef) extends Actor with ActorLogging {
   import TCPClient._
 
   implicit val timeout = Timeout(5.seconds)
@@ -28,19 +27,19 @@ class TCPClient(ip: String, port: Int, peer: ActorRef) extends Actor with ActorL
   def receive = {
     case IO.Closed(socket, cause) =>
       log.info(s"connection to ${socket} closed: ${cause}")
-      peer ! ConnectionClosed
+      btProtocol ! ConnectionClosed
       socket.close
     case IO.Read(_, bytes) =>
       buffer = buffer ++ bytes
       var bytesRead = 0
       do {
-        bytesRead = Await.result(peer ? DataReceived(buffer), 5.seconds).asInstanceOf[Int]
+        bytesRead = Await.result(btProtocol ? DataReceived(buffer), 5.seconds).asInstanceOf[Int]
         buffer = buffer.drop(bytesRead)
       } while (bytesRead > 0)
     case SendData(bytes) =>
       socket write bytes
     case CloseConnection =>
-      peer ! ConnectionClosed
+      btProtocol ! ConnectionClosed
       socket.close
   }
 }
