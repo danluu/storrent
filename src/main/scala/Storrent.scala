093bf446280b431d37afb822f47a0a63dc6e8a08
Dan Luu <danluu@gmail.com>
2013-04-17 12:59:50 -0400

    Add usage

--- src/main/scala/Storrent.scala
+++ src/main/scala/Storrent.scala
@@ -5,6 +5,10 @@ import akka.actor.{ Actor, ActorRef, Props, ActorSystem }
 object Storrent {
   val system = ActorSystem("storrent")
   def main(args: Array[String]) {
-    args.foreach { f => system.actorOf(Props(new Torrent(f)), s"Torrent${f}") }
+    if(args.isEmpty) {
+      println("Usage: Storrent [torrent file] [torrent file] ...")
+    } else {
+      args.foreach { f => system.actorOf(Props(new Torrent(f)), s"Torrent${f}") }
+    }
   }
 }
